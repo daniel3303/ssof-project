3377ba8c7808afc8f62e27ba8688ce3834066009
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2018-11-15 00:59:51 +0000

    Some StackManager changes

--- context.py
+++ context.py
@@ -1,15 +1,22 @@
+from stack import *
+
 class Context:
 	def __init__(self):
+		# TODO move register values to a stack object. The context will
+		# keep a list of all the stacks and will pop one when there is a ret instruction
+		self.stackManager = StackManager()
+
 		#self.variables = {} vars globais
-		
+
 		self.functions = {}
 		self.vulnerabilities = []
+
 		#register values in hex
 		self.registers = {'r14': "0x0", 'r15': "0x0", 'rcx': "0x0", 'rsi': "0x0",
 						'r10': "0x0", 'rbx': "0x0", 'rdi': "0x0", 'r11': "0x0",
 						'r8': "0x0", 'rdx': "0x0", 'rip': "0x0", 'r9': "0x0",
 						'r12': "0x0", 'rbp': "0x0", 'rsp': "0x0", 'rax': "0x0", 'r13': "0x0" }
-		self.current_function = "main"
+		self.currentFunction = "main"
 
 	def addFunction(self, function):
 		self.functions[function.name] = function
@@ -24,3 +31,9 @@ class Context:
 		string = "Registers: "
 		for key in self.registers:
 			print("key:{} value:{}".format(key, self.registers[key]))
+
+	def getStackManager(self):
+		return self.stackManager
+
+	def createStack(self):
+		return self.stackManager.createStack()
--- stack.py
+++ stack.py
@@ -16,3 +16,16 @@ class Stack:
 class StackManager:
     def __init__(self, function):
         self.stacks = []
+
+    def createStack(self):
+        self.stacks.append(Stack())
+        return True
+
+
+    # Removes and returns the last element from the stacks lists.
+    # @Return None if empty
+    def popStack(self):
+        if len(self.stacks) < 1:
+            return None
+
+        return self.stacks.pop()
