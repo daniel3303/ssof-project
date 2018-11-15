839df92c28a5ae31236ca00bc39de6ceb5d375e1
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2018-11-15 05:04:50 +0000

    Variable changes

--- variable.py
+++ variable.py
@@ -4,6 +4,7 @@ class Variable:
 		self.type = var_type
 		self.size = size
 		self.address = address
+		self.executionAddress = ""
 		self.effectiveSize = size # size when simulating input, for example fgets size lower than max size
 
 	def getName(self):
@@ -20,3 +21,9 @@ class Variable:
 
 	def setAddress(self, newAddress):
 		self.address = newAddress
+
+	def getExecutionAddress(self):
+		return self.executionAddress
+
+	def setExecutionAddress(self, newAddress):
+		self.executionAddress = newAddress
